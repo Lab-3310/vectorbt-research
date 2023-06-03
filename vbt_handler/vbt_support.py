21d85cab5a0736588551bb8b4f23561afb169f95
WilliamChou <31893928+william60902@users.noreply.github.com>
2023-06-04 00:22:16 +0800

    flex in save dir

--- vbt_handler/vbt_support.py
+++ vbt_handler/vbt_support.py
@@ -2,7 +2,6 @@ import os
 import pandas as pd
 
 def vbt_saver_printer(
-    user_home_path: str,
     save_dir: str, 
     strategy_logic: str, 
     strategy_config: str,
@@ -13,23 +12,29 @@ def vbt_saver_printer(
     asset_return: pd.DataFrame,
     asset_value: pd.DataFrame,
     cumulative_returns: pd.DataFrame
-    ):
+):
 
-    save_dir = f'{user_home_path}/backtest-engine-alpha/data_center/vector_bt'
     os.makedirs(save_dir, exist_ok=True)
 
-    os.makedirs(save_dir + f"/{strategy_logic}/stats/{strategy_config}", exist_ok=True)
-    os.makedirs(save_dir + f"/{strategy_logic}/position/{strategy_config}", exist_ok=True)
-    os.makedirs(save_dir + f"/{strategy_logic}/metric_df/{strategy_config}", exist_ok=True)
-    os.makedirs(save_dir + f"/{strategy_logic}/asset/{strategy_config}", exist_ok=True)
-    os.makedirs(save_dir + f"/{strategy_logic}/trade_pnl/{strategy_config}", exist_ok=True)
-    # saving
-    stats_df.to_csv(save_dir + f"/{strategy_logic}/stats" + f"/{strategy_config}/{strategy_config}_stats_vbt.csv")
-    position_df.to_csv(save_dir + f"/{strategy_logic}/position"+ f"/{strategy_config}/{strategy_config}_position_vbt.csv")
-    bt_df.to_csv(save_dir + f"/{strategy_logic}/metric_df" + f"/{strategy_config}/{strategy_config}_metric_df_vbt.csv")
+    stats_dir = f"{save_dir}/{strategy_logic}/stats/{strategy_config}"
+    os.makedirs(stats_dir, exist_ok=True)
+    position_dir = f"{save_dir}/{strategy_logic}/position/{strategy_config}"
+    os.makedirs(position_dir, exist_ok=True)
+    metric_df_dir = f"{save_dir}/{strategy_logic}/metric_df/{strategy_config}"
+    os.makedirs(metric_df_dir, exist_ok=True)
+    asset_dir = f"{save_dir}/{strategy_logic}/asset/{strategy_config}"
+    os.makedirs(asset_dir, exist_ok=True)
+    trade_pnl_dir = f"{save_dir}/{strategy_logic}/trade_pnl/{strategy_config}"
+    os.makedirs(trade_pnl_dir, exist_ok=True)
+
+    # Saving dataframes
+    stats_df.to_csv(f"{stats_dir}/{strategy_config}_stats_vbt.csv")
+    position_df.to_csv(f"{position_dir}/{strategy_config}_position_vbt.csv")
+    bt_df.to_csv(f"{metric_df_dir}/{strategy_config}_metric_df_vbt.csv")
     trade_record = vbt_runner.positions.records_readable
-    trade_record.to_csv(save_dir + f"/{strategy_logic}/trade_pnl" + f"/{strategy_config}/{strategy_config}_trade_pnl_vbt.csv")
-    asset_return.to_csv(save_dir + f"/{strategy_logic}/asset" + f"/{strategy_config}/{strategy_config}_asset_return_vbt.csv")
-    asset_value.to_csv(save_dir + f"/{strategy_logic}/asset" + f"/{strategy_config}/{strategy_config}_asset_value_vbt.csv")
-    cumulative_returns.to_csv(save_dir + f"/{strategy_logic}/asset" + f"/{strategy_config}/{strategy_config}_cumulative_returns_vbt.csv")
+    trade_record.to_csv(f"{trade_pnl_dir}/{strategy_config}_trade_pnl_vbt.csv")
+    asset_return.to_csv(f"{asset_dir}/{strategy_config}_asset_return_vbt.csv")
+    asset_value.to_csv(f"{asset_dir}/{strategy_config}_asset_value_vbt.csv")
+    cumulative_returns.to_csv(f"{asset_dir}/{strategy_config}_cumulative_returns_vbt.csv")
+
     print("VBT Success!")
