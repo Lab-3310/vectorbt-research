f41aad846fe26ee8bd505ebc79dfaf9ec48feef3
Harper1013 <31893928+william60902@users.noreply.github.com>
2023-06-05 00:54:12 +0800

    win update

--- binance_loader/script/window_ops/win_daily_binance_update.sh
+++ binance_loader/script/window_ops/win_daily_binance_update.sh
@@ -1,2 +1,2 @@
-python3 binance_loader\binance_updater.py --symbol_list select --timeframe 1d --product SPOT
-python3 binance_loader\binance_updater.py --symbol_list select --timeframe 1d --product UPERP
+python binance_loader\binance_updater.py --symbol_list select --timeframe 1d --product SPOT
+python binance_loader\binance_updater.py --symbol_list select --timeframe 1d --product UPERP
---
+++ binance_loader/script/window_ops/win_minute_binance_update.sh
@@ -0,0 +1,2 @@
+python binance_loader\binance_updater.py --symbol_list select --timeframe 1m --product SPOT
+python binance_loader\binance_updater.py --symbol_list select --timeframe 1m --product UPERP
