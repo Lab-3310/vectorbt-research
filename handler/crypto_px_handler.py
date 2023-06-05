43f08d20650e02e09ed426c46085ca477afe7c4a
Harper1013 <31893928+william60902@users.noreply.github.com>
2023-06-05 22:49:00 +0800

    window path fix

--- handler/crypto_px_handler.py
+++ handler/crypto_px_handler.py
@@ -1,5 +1,5 @@
 import os
-
+import platform
 import pandas as pd
 import configparser
 
@@ -12,10 +12,16 @@ def get_crypto_data_df(symbol, use_spot=False, resample_p=False):
 
     if use_spot: # for crypto default use_spot as false is using uperp
         spot_path = path_config.get('crypto', 'binance_spot')
-        min_data = pd.read_csv(spot_path + f'/{symbol}_1m.csv', index_col='datetime')
+        if platform.system() == "Darwin":
+            min_data = pd.read_csv(spot_path + f'/{symbol}_1m.csv', index_col='datetime')
+        else:
+            min_data = pd.read_csv(spot_path + f'\{symbol}_1m.csv', index_col='datetime')
     else:
         uperp_path = path_config.get('crypto', 'binance_uperp')
-        min_data = pd.read_csv(uperp_path + f'/{symbol}_1m.csv', index_col='datetime')
+        if platform.system() == "Darwin":
+            min_data = pd.read_csv(uperp_path + f'/{symbol}_1m.csv', index_col='datetime')
+        else:
+            min_data = pd.read_csv(uperp_path + f'\{symbol}_1m.csv', index_col='datetime')
 
     min_data.index = pd.to_datetime(min_data.index, format='%Y-%m-%d %H:%M:%S')
     data_df = resample_data(min_data, resample_p)
