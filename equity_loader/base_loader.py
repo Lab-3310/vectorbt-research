7809180724003601c88cbfaf326bc8219500b405
Harper <drizzle60902@gmail.com>
2023-06-10 02:42:20 +0800

    sino broker & sino loader init

---
+++ equity_loader/base_loader.py
@@ -0,0 +1,22 @@
+import datetime
+import logging
+import time
+from datetime import timedelta
+from enum import Enum
+import pandas as pd
+
+class SinoBroker(Enum):
+    SINO = 'sino'
+    TWEQ = 'tweq'
+    FUTURE = 'future'
+    PERSON_ID = 'person_id'
+    PASSWD = 'pass_wd'
+
+
+class BaseLoader:
+    def __init__(self):
+        self._get_broker_config = {}
+
+    @property
+    def get_broker_config(self):
+        return self._get_broker_config
