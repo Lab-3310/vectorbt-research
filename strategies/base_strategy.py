43ca4a1f3788876545a38c8c2da71158665146f5
WilliamChou <31893928+william60902@users.noreply.github.com>
2023-06-17 07:07:26 +0800

    self.generate_backtest_result

--- strategies/base_strategy.py
+++ strategies/base_strategy.py
@@ -4,6 +4,8 @@ import logging
 import pandas as pd
 import types_enums.base_strategy_enum as base_strategy_enum
 from types_enums.vbt_enum import *
+import plotly.express as px
+import vectorbt as vbt
 
 class BaseStrategy:
     def __init__(self, strategy_class: str, strategy_config: str):
@@ -58,6 +60,19 @@ class BaseStrategy:
     def run_backtest(self):
         self.logger.info(f'Running Backtest - [{self._strategy_class=}, {self._strategy_config=}].')
 
+    def vbt_init(self):
+        vbt_func = vbt.Portfolio.from_signals(init_cash=self.capital, sl_stop=self.SL_pct, tp_stop=self.TP_pct, close=self.backtest_df[CLOSE], entries=self.backtest_df[ENTRY_LONG], exits=self.backtest_df[EXIT_SHORT], short_entries=self.backtest_df[ENTRY_SHORT], short_exits=self.backtest_df[EXIT_SHORT], direction='both', accumulate=False, freq='d')
+        vbt_func.plot().show()
+        position_df = pd.DataFrame({DATETIME:vbt_func.asset_flow().index, POSITION:vbt_func.asset_flow().values})
+        position_fig = px.line(position_df, x=DATETIME, y=POSITION)
+        position_fig.show()
+
+        stats_df = vbt_func.stats()
+        asset_return = vbt_func.asset_returns()
+        asset_value = vbt_func.asset_value()
+        cumulative_returns = vbt_func.cumulative_returns()
+        return vbt_func, stats_df, asset_return, asset_value, cumulative_returns, position_df
+    
     def vbt_result(self, strategy_class: str, strategy_config: str, stats_df: pd.DataFrame, position_df: pd.DataFrame, backtest_df: pd.DataFrame, vbt_func, asset_return: pd.DataFrame, asset_value: pd.DataFrame, cumulative_returns: pd.DataFrame):
         save_dir = f'{os.path.dirname(__file__)}/../backtest_result/{strategy_class}/{strategy_config}'
         os.makedirs(save_dir, exist_ok=True)
@@ -75,8 +90,11 @@ class BaseStrategy:
         print(stats_df)
         print('---------')
         print(f'VBT Backtest Report in {strategy_class}/{strategy_config} Saved Success!')
-
-
+    
+    def generate_backtest_result(self):
+        vbt_func, stats_df, asset_return, asset_value, cumulative_returns, position_df = self.vbt_init()
+        self.vbt_result(self._strategy_class, self._strategy_config, stats_df, position_df, self.backtest_df, vbt_func, asset_return, asset_value, cumulative_returns)
+              
 
 
 
