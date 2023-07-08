b4b2f0aa3025f483fea2611d5e5d5089103efb45
WilliamChou <31893928+william60902@users.noreply.github.com>
2023-07-08 19:28:55 +0800

    Update binance_px.py

--- binance_loader/binance_ccxt/binance_px.py
+++ binance_loader/binance_ccxt/binance_px.py
@@ -102,22 +102,27 @@ def get_start_dt(symbols_detail_map, symbol):
     return start_dt
 
 def binance_loader():
-
     '''
     --symbol_list select --timeframe 1d --product UPERP
-
     '''
 
     sys.path.append(f'{root_path}/vectorbt-research')
 
+    # Get the path to the config.ini file based on the user's operating system
+    if platform.system() == "Darwin":
+        config_path = os.path.expanduser('~/vectorbt-research/config/config.ini')
+    elif platform.system() == "Windows":
+        config_path = os.path.expanduser(r'C:\Users\<username>\vectorbt-research\config\config.ini')
+
     download_config = configparser.ConfigParser()
-    # different ops read different config path
+    # Load the config.ini file
+    download_config.read(config_path)
+
+    # Retrieve the value based on the platform
     if platform.system() == "Darwin":
         binance_download_path = download_config.get('binance', 'mac_path')
     elif platform.system() == "Windows":
         binance_download_path = download_config.get('binance', 'window_path')
-
-    binance_download_path = download_config.get('binance', 'path')
 
     # create the database path file you designated
     os.makedirs(binance_download_path, exist_ok=True)
