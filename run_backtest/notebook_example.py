65d1ee90a110d7c24a22687a1edfa7f7017e2d4e
WilliamChou <31893928+william60902@users.noreply.github.com>
2023-07-13 21:04:11 +0800

    Create notebook_example.py

---
+++ run_backtest/notebook_example.py
@@ -0,0 +1,14 @@
+# This is an exampel for user to use in notebook 
+
+
+
+# import sys, os
+# sys.path.insert(1, '/Users/chouwilliam/vectorbt-research')
+
+# from strategies.dmi_strategy import DMIStrategy
+
+# strategy_class = 'DMIStrategy'
+# strategy_config = 'dmi_config_01'
+
+# strategy = DMIStrategy(strategy_class, strategy_config)
+# strategy.run_backtest()
