37851c6cd5614f812e3ed09f2860595a51daa4f5
Allen-chen-hub <132655313+Allen-chen-hub@users.noreply.github.com>
2023-09-21 22:21:02 +0800

    graph (#25)

---
+++ backoffice/Graph.py
@@ -0,0 +1,42 @@
+import vectorbt as vbt
+import pandas as pd 
+import numpy as np
+import datetime
+
+
+
+symbols = ["BTC-USD"]
+price = vbt.YFData.download(symbols, missing_index='drop').get('Close')
+
+def showQS(pf):
+    vbt.settings.array_wrapper.freq = 'D'
+    pf.qs.plots_report()
+
+    return None
+
+def candal_graph(symbols):
+    price_close=vbt.YFData.download(symbols).get('Close')
+    price_high=vbt.YFData.download(symbols).get('High')
+    price_open=vbt.YFData.download(symbols).get('Open')
+    price_low=vbt.YFData.download(symbols).get('Low')
+    price_volume=vbt.YFData.download(symbols).get('Volume')
+    
+    df = pd.DataFrame({
+    'my_open1':price_open,
+    'my_high2': price_high,
+    'my_low3': price_low,
+    'my_close4':price_close,
+    'my_volume5': price_volume
+    })
+    my_column_names = dict(
+    open='my_open1',
+    high='my_high2',
+    low='my_low3',
+    close='my_close4',
+    volume='my_volume5',
+    )
+
+    ohlcv_acc = df.vbt.ohlcv(freq='d', column_names=my_column_names)
+    ohlcv_acc.plots(settings=dict(plot_type='candlestick'))
+
+    return None
