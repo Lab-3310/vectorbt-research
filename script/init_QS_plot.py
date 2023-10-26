b34bdc4a4efe2900c757b988358bef8af0d1999f
Allen-chen-hub <132655313+Allen-chen-hub@users.noreply.github.com>
2023-10-26 21:46:32 +0800

    1026

---
+++ script/init_QS_plot.py
@@ -0,0 +1,23 @@
+import os
+import sys
+
+sys.path.insert(1, os.path.expanduser('~/Systematic-Sherpa')) # TODP path fix general way
+
+from backoffice.vbt_plot.stats_report import startegy_stat
+
+def main():
+    # sample strategy
+    price = vbt.YFData.download('BTC-USD').get('Close')
+
+    fast_ma = vbt.MA.run(price, 10)
+    slow_ma = vbt.MA.run(price, 50)
+    entries = fast_ma.ma_crossed_above(slow_ma)
+    exits = fast_ma.ma_crossed_below(slow_ma)
+
+    pf = vbt.Portfolio.from_signals(price, entries, exits, init_cash=100)
+    stat_activate = startegy_stat(pf)
+    stat_activate.show_stats()
+
+
+if __name__ == '__main__':
+    main()
